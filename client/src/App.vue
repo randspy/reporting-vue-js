c1915112b3cdd2fb86a49932878820556b33f389
Przemyslaw Koziel <mos.koziel@gmail.com>
2025-03-23 07:46:12 +0100

    chore: initial commit - copied from vuejs starter project

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen gap-4">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <Button>Click me</Button>
+  </div>
+</template>
+
+<style></style>
